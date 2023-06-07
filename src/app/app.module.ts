50479081ca12ed643d992d7821c9f7e1870dc7f3
jonanv <jonan-vargas23@hotmail.com>
2023-06-07 15:59:46 -0500

    :wrench: (app): Add modules and configuration for ripple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Modules
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RippleModule } from 'primeng/ripple';
+import { ButtonModule } from 'primeng/button';
+
+// Components
+import { DashboardComponent } from './modules/dashboard/pages/dashboard.component';
+import { HomeComponent } from './modules/dashboard/components/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RippleModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
